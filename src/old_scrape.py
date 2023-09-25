9397ec65c1ddc3d06e3c6c4b9738248a4ad93644
Paul Dilly <paul.dilly@duke.edu>
2023-09-25 18:19:24 -0400

    fixed scraping function

--- src/old_scrape.py
+++ src/old_scrape.py
@@ -13,7 +13,7 @@ directory = os.path.dirname(cwd)
 parent = os.path.join(directory, "chromedriver")
 dict = {}
 
-def scrape(url, file):
+def scrape(url, file, num_obs):
 
     def scroll_to_bottom(driver):
         old_position = 0
@@ -39,16 +39,44 @@ def scrape(url, file):
 
     with chrome_driver as driver:
 
-        driver.maximize_window() 
-        driver.get(url) # setting driver's url
-        time.sleep(3) # waiting for page to load completely
-        scroll_to_bottom(driver) # scrolling to bottom of page to capture all rows in table
-        table = driver.find_element(By.XPATH, "//*[@id='Col1-1-HistoricalDataTable-Proxy']/section/div[2]/table/tbody") # scraping entire table
-        with open('table.txt', 'w') as table_file: # writing text contents of table to .txt file
-            table_file.write(table.text)
-        df = pandas.read_csv('table.txt', sep=" ", header=0) # loading .txt file as a data frame
-        df.columns = ['Month', 'Day', 'Year', 'Open', 'High', 'Low', 'Close', 'Adj_Close', 'Volume'] # adding column headers to data frame
-        df.to_csv(os.path.join(directory, 'data', file)) # saving data frame to .csv with provided file name
-        os.remove('table.txt') # removing .txt file
+        driver.maximize_window()
+        driver.get(url)
+        time.sleep(3)
+        scroll_to_bottom(driver)
+        for i in range(1, 8):
+            dict[i] = []
+
+        rows = 0
+        max_num = 0
+        scraped_rows = driver.find_elements(By.XPATH, "//*[@id='Col1-1-HistoricalDataTable-Proxy']/section/div[2]/table/tbody")
+        for row in scraped_rows:
+            curr = (int) (len(row.text) / 70)
+            max_num = max(curr, max_num)
+        if num_obs is None:
+            rows = max_num
+        else:
+            rows = (int) (min(num_obs, max_num))
+        break_flag = False
+
+        for row in range(1, rows):
+            for col in range(1, 8):
+                try: 
+                    cell = driver.find_element(By.XPATH, "//*[@id='Col1-1-HistoricalDataTable-Proxy']/section/div[2]/table/tbody/tr["+str(row)+"]/td["+str(col)+"]")
+                    dict[col].append(cell.text)
+                except NoSuchElementException:
+                    break_flag = True
+                    break
+            if break_flag:
+                break
+
+        min_num = rows + 1000
+        for key in dict:
+            min_num = min(len(dict[key]), min_num)
+        for key in dict:
+            while len(dict[key]) > min_num:
+                dict[key].pop(-1)
+
+        df = pandas.DataFrame.from_dict(dict)
+        df.to_csv(os.path.join(directory, 'data', file))
 
     driver.quit()
