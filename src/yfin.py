69adcc83ddc6f2a2a1a1f04f237d784d1be51c22
Paul Dilly <paul.dilly@duke.edu>
2023-10-22 16:50:49 -0400

    rmvd files, updated query_yahoo

--- src/yfin.py
+++ src/yfin.py
@@ -8,6 +8,7 @@ directory = os.path.dirname(cwd)
 tickers = ["^GSPC", "^DJI", "^IXIC", "^NYA", "^XAX", "^BUK100P", "^RUT", "^VIX", "^FTSE", "^GDAXI", "^FCHI", "^STOXX50E", "^N100", "^BFX", "IMOEX.ME",
            "^N225", "HSI", "000001.SS", "399001.SZ", "^STI", "^AXJO", "^AORD", "^BSESN", "^JKSE", "^KLSE", "^NZ50", "^KS11", "^TWII", "^GSPTSE", "^BVSP",
            "^MXX", "^IPSA", "^MERV", "^TA125.TA", "^JN0U.JO"]
+file_names = []
 
 def query_yahoo():
   for ticker in tickers:
@@ -17,4 +18,7 @@ def query_yahoo():
           continue
       else: # save historical data to .csv
           ticker = ticker.replace("^", "")
-          df.to_csv(os.path.join(directory, 'data', f'ticker_{ticker}.csv'))
+          file = f'ticker_{ticker}.csv'
+          file_names.append(file)
+          df.to_csv(os.path.join(directory, 'data', file))
+  return file_names
